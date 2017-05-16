32042b6babed6d4e7ca575593fd1bace71ca9d35
Shamila <shamila.m09@gmail.com>
2017-05-15 19:01:12 -0700

    static site complete

--- client/controllers/portfolioCtrl.js
+++
@@ -1,9 +0,0 @@
-(function (angular){
-
-	.module('portfolioApp')
-	.controller('portfolioCtrl');
-
-	function homeCtrl($scope, $location){
-		var vm = this;
-	}
-} ) (angular);
--- client/controllers/skillsCtrl.js
+++
@@ -1,9 +0,0 @@
-(function (angular){
-
-	.module('portfolioApp')
-	.controller('skillsCtrl');
-
-	function skillsCtrl($scope, $location){
-		var vm = this;
-	}
-} ) (angular);
