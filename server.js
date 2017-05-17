b530ad8a3aa3729292762178a75829cd2e74dab7
Shamila <shamila.m09@gmail.com>
2017-05-16 22:34:58 -0700

    cleaned

--- server.js
+++ server.js
@@ -2,45 +2,11 @@
 var express = require('express');
 var app = express();
 var bodyParser	= require('body-parser');
-var nodemailer = require('nodemailer');
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client'));
 
 
-// SMTP starts
-var smtpTransport = nodemailer.createTransport({
-	service: "gmail",
-	host: "smtp.gmail.com",
-	auth: {
-		user: "",
-		pass: ""
-	}
-});
-// SMTP ends
-
-//routing starts
-
-
-app.post('/contact-form', function(req,res){
-	var data = req.body;
-
-	var mailOptions ={
-		from : data.contactName,
-		to : "shamila.m09@gmail.com", //msg destination
-		subject : 'Message from'+' '+data.contactName+', Email: '+ data.contactEmail,
-		text : data.contactMsg
-	}
-	// console.log(mailOptions);
-	smtpTransport.sendMail(mailOptions, function(error, response){
-		if(error){
-			// console.log(error);
-			res.end("error");
-		}else{
-			res.end("sent");
-		};
-	});
-});
 
 app.listen(8000, function() {
 	console.log('Server is listening on port 8000');
